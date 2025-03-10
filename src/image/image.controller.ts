0cd45a2109a8d6b5612c1a24bc70fd3a69566977
Akash Mondal <akash2003modal@gmail.com>
2025-03-10 20:27:05 +0530

    Add RedisModule import to ImageModule

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,82 +1,35 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Res, Query, HttpException, HttpStatus, Logger, UseGuards, UseInterceptors, UploadedFiles } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res, Query, HttpException, HttpStatus, Logger, UseGuards, UseInterceptors, UploadedFiles, Req } from '@nestjs/common';
 import { ImageService } from './image.service';
-import { CreateImageDto } from './dto/create-image.dto';
-import { UpdateImageDto } from './dto/update-image.dto';
-import { supabase } from 'src/lib/Supabase';
 import { FastifyReply } from 'fastify';
 import { FilesInterceptor } from '@nest-lab/fastify-multer';
 import { MyAuthGuard } from 'src/auth/guard/My-jwt-auth.guard';
 import { RestApiSessionUser } from 'src/decorator/session.decorator';
 import { Author } from 'src/users/entities/author.entity';
 import { ReqFile } from './entities/image.entity';
-import sharp from 'sharp';
 
 @Controller('image')
 export class ImageController {
   constructor(private readonly imageService: ImageService) { }
 
-  @Post()
-  create(@Body() createImageDto: CreateImageDto) {
-    return this.imageService.create(createImageDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.imageService.findAll();
-  }
-
   @Post('/upload')
   @UseGuards(MyAuthGuard)
   @UseInterceptors(FilesInterceptor('files', 5, { limits: { fileSize: 20 * 1024 * 1024 } }))
   async uploadImage(@UploadedFiles() files: ReqFile[], @RestApiSessionUser() session: Author) {
-    try {
-      const data = await this.imageService.compressedImages(files, session.id)
-      return data;
-    } catch (error) {
-      console.log(error)
-      throw new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+    const data = await this.imageService.uploadImage(files, session.id)
+    return data;
   }
 
-  @Get(':id')
+  @Get(':id/:path')
   async findImage(
     @Param('id') id: string,
+    @Param('path') path: string,
     @Res() res: FastifyReply,
     @Query("w") w?: string,
     @Query("h") h?: string,
     @Query("q") q?: string,
   ) {
-    try {
-      // Get the original image from the Supabase bucket
-      const { data: originalImage, error: downloadError } = await supabase.storage.from('snaapio-production').download(id);
-      if (downloadError || !originalImage) {
-        throw new HttpException(`Failed to download image: ${downloadError?.message}`, HttpStatus.NOT_FOUND);
-      }
-
-      // Convert original image to Buffer
-      const imageBuffer = Buffer.from(await originalImage.arrayBuffer());
-
-      // Optimize the image using sharp
-      const optimizedImage = await sharp(imageBuffer)
-        .resize(w ? Number(w) : undefined, h ? Number(h) : undefined) // Allow optional resizing
-        .jpeg({ quality: q ? Number(q) : 70 }) // Default quality if not provided
-        .toBuffer();
-
-      // Send the optimized image
-      res.type("image/jpeg").send(optimizedImage);
-    } catch (error) {
-      Logger.error("Image processing failed:", error);
-      throw new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+    const optimizedImage = await this.imageService.imageOptimization({ id,path, w, h, q });
+    res.type("image/jpeg").send(optimizedImage);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateImageDto: UpdateImageDto) {
-    return this.imageService.update(+id, updateImageDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.imageService.remove(+id);
-  }
 }
--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -1,9 +1,8 @@
 import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
-import { CreateImageDto } from './dto/create-image.dto';
-import { UpdateImageDto } from './dto/update-image.dto';
 import { supabase } from 'src/lib/Supabase';
-import sharp from 'sharp';
 import { ReqFile } from './entities/image.entity';
+import { RedisProvider } from 'src/db/redis/redis.provider';
+import sharp from 'sharp';
 
 const imageVariants = [
   { aspectRatio: "1.1-sm", width: 100, height: 100, quality: 70 },
@@ -18,100 +17,172 @@ const imageBlurVariants = [
 
 @Injectable()
 export class ImageService {
-  create(createImageDto: CreateImageDto) {
-    return 'This action adds a new image';
-  }
+  constructor(
+    // private readonly drizzleProvider: DrizzleProvider,
+    private readonly redisProvider: RedisProvider,
+  ) { }
+  expire: number = 7 * 24 * 60 * 60;
 
-  findAll() {
-    return `This action returns all image`;
-  }
+  // function depend -> compressedImages
+  async processAndUploadImage(
+    file: ReqFile,
+    variant: { width: number; height: number, quality: number, aspectRatio: string },
+    blur: boolean,
+    userId: string
+  ): Promise<string | null> {
+    try {
+      let image = sharp(file.buffer).resize({
+        width: variant.width,
+        height: variant.height,
+        fit: "cover",
+      });
 
-  findOne(id: number) {
-    return `This action returns a #${id} image`;
-  }
+      if (blur) {
+        image = image.blur(16).jpeg({ quality: variant.quality });
+      } else {
+        image = image.jpeg({ quality: variant.quality });
+      }
 
-  update(id: number, updateImageDto: UpdateImageDto) {
-    return `This action updates a #${id} image`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} image`;
-  }
-
-    // compressed
-    async processAndUploadImage(
-      file: ReqFile,
-      variant: { width: number; height: number, quality: number, aspectRatio: string },
-      blur: boolean,
-      userId: string
-    ): Promise<string | null> {
-      try {
-        let image = sharp(file.buffer).resize({
-          width: variant.width,
-          height: variant.height,
-          fit: "cover",
+      const compressedImage = await image.toBuffer();
+      const filePath = `${userId}_${file.originalname}`;
+      const path = `${blur ? `${variant.aspectRatio}-blur` : variant.aspectRatio}/${filePath}`;
+      const { error, data } = await supabase.storage
+        .from("snaapio-production")
+        .upload(path, compressedImage, {
+          cacheControl: "3600",
+          contentType: "image/jpeg",
+          upsert: false,
         });
-  
-        if (blur) {
-          image = image.blur(16).jpeg({ quality: variant.quality });
-        } else {
-          image = image.jpeg({ quality: variant.quality });
-        }
-  
+
+      if (error) {
+        Logger.error(`Failed to upload ${filePath}:`, error);
+        return null;
+      }
+
+      return filePath;
+    } catch (error) {
+      Logger.error(`Processing error for ${file.originalname}:`, error);
+      return null;
+    }
+  }
+
+  async compressedImages(files: ReqFile[], userId: string): Promise<string[]> {
+    let imgArr: string[] = [];
+
+    try {
+      // Process normal images
+      const uploadPromises = files.map(async (file) => {
+        const urls = await Promise.all(
+          imageVariants.map((variant) => this.processAndUploadImage(file, variant, false, userId))
+        );
+        imgArr.push(...urls.filter((url): url is string => url !== null)); // Filter out null values
+      });
+
+      // Process blurred images
+      const uploadImageBlurPromises = files.map(async (file) => {
+        const urls = await Promise.all(
+          imageBlurVariants.map((variant) => this.processAndUploadImage(file, variant, true, userId))
+        );
+        imgArr.push(...urls.filter((url): url is string => url !== null)); // Filter out null values
+      });
+
+      // Wait for all uploads to finish
+      await Promise.all([...uploadPromises, ...uploadImageBlurPromises]);
+
+      return imgArr; // Return the collected image URLs
+    } catch (error) {
+      Logger.error("Image compression failed:", error);
+      throw new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+  // new
+  async uploadImage(files: ReqFile[], userId: string) {
+    try {
+      let imgUrls: string[] = [];
+      const uploadPromises = files.map(async (file) => {
+        const filePath = `${userId}/${file.originalname}`;
+        const image = sharp(file.buffer).jpeg({ quality: 60 });
         const compressedImage = await image.toBuffer();
-        const filePath = `${userId}_${file.originalname}`;
-        const path = `${blur ? `${variant.aspectRatio}-blur` : variant.aspectRatio}/${filePath}`;
-        const { error, data } = await supabase.storage
+
+        const { error } = await supabase.storage
           .from("snaapio-production")
-          .upload(path, compressedImage, {
+          .upload(filePath, compressedImage, {
             cacheControl: "3600",
             contentType: "image/jpeg",
             upsert: false,
           });
-  
+
         if (error) {
-          Logger.error(`Failed to upload ${filePath}:`, error);
-          return null;
+          if (error?.message === "The resource already exists") {
+            // Logger.error(`Failed to upload ${filePath}: ${error.message}`);
+            return filePath;
+          };
         }
-  
-        return filePath;
-      } catch (error) {
-        Logger.error(`Processing error for ${file.originalname}:`, error);
-        return null;
+        // Store in Redis (convert to Base64 to prevent binary storage issues)
+        await this.redisProvider.client.set(`${filePath.replace("/", ":")}`, file.buffer.toString('base64'), "EX", this.expire); // ----------- set image to redis
+        return filePath; // Return filePath for successful uploads
+      });
+
+      imgUrls = (await Promise.all(uploadPromises))
+
+      return imgUrls;
+    } catch (error) {
+      Logger.error("Upload Processing error:", error);
+      return null;
+    }
+  }
+
+  async imageOptimization({ id, w, h, q, path, }: { id: string; w?: string; h?: string; q?: string; path: string; }) {
+    try {
+      if (!id || !path) {
+        throw new HttpException('Invalid parameters: id and path are required', HttpStatus.BAD_REQUEST);
       }
-    }
-  
-    async compressedImages(files: ReqFile[], userId: string): Promise<string[]> {
-      let imgArr: string[] = [];
-  
-      try {
-        // Process normal images
-        const uploadPromises = files.map(async (file) => {
-          const urls = await Promise.all(
-            imageVariants.map((variant) => this.processAndUploadImage(file, variant, false, userId))
-          );
-          imgArr.push(...urls.filter((url): url is string => url !== null)); // Filter out null values
-        });
-  
-        // Process blurred images
-        const uploadImageBlurPromises = files.map(async (file) => {
-          const urls = await Promise.all(
-            imageBlurVariants.map((variant) => this.processAndUploadImage(file, variant, true, userId))
-          );
-          imgArr.push(...urls.filter((url): url is string => url !== null)); // Filter out null values
-        });
-  
-        // Wait for all uploads to finish
-        await Promise.all([...uploadPromises, ...uploadImageBlurPromises]);
-  
-        return imgArr; // Return the collected image URLs
-      } catch (error) {
-        Logger.error("Image compression failed:", error);
-        throw new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR);
+
+      const createUrl = `${id}:${path}?w=${w || ''}&h=${h || ''}&q=${q || ''}`;
+
+      // Check Redis Cache
+      const cacheImage = await this.redisProvider.client.get(createUrl);  // ----------- get image to redis
+      if (cacheImage) {
+        // console.log("query cache hit");
+        return Buffer.from(cacheImage, 'base64'); // Ensure correct conversion from Base64
       }
+      let imageBuffer: any;
+      // find original image cache
+      const originalCacheImage = await this.redisProvider.client.get(`${id}:${path}`); // ----------- get image to redis
+
+      if (!originalCacheImage) {
+        // console.log("original hit");
+        // Get the original image from Supabase
+        const { data: originalImage, error: downloadError } = await supabase.storage
+          .from('snaapio-production')
+          .download(`${id}/${path}`);
+
+
+        if (downloadError || !originalImage) {
+          throw new HttpException(`Failed to download image: ${downloadError?.message || 'Unknown error'}`, HttpStatus.NOT_FOUND);
+        }
+
+        // Convert original image to Buffer
+        imageBuffer = Buffer.from(await originalImage.arrayBuffer());
+        await this.redisProvider.client.set(`${id}:${path}`, imageBuffer.toString('base64'), "EX", this.expire); // ----------- set original image to redis
+      } else {
+        // console.log("original cache hit");
+        imageBuffer = Buffer.from(originalCacheImage, 'base64');
+      }
+
+      // Optimize the image using sharp
+      const optimizedImage = await sharp(imageBuffer)
+        .resize(w ? Number(w) : undefined, h ? Number(h) : undefined)
+        .jpeg({ quality: q ? Number(q) : 70 })
+        .toBuffer();
+
+      // Store in Redis (convert to Base64 to prevent binary storage issues)
+      await this.redisProvider.client.set(createUrl, optimizedImage.toString('base64'), "EX", this.expire); // ----------- set image to redis
+
+      return optimizedImage;
+    } catch (error) {
+      Logger.error("Image processing failed:", error);
+      throw new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR);
     }
-  
-    async imageOptimization({ id, w, h, q }: { id: string, w: string, h: string, q: string }) {
-     
-    }
+  }
 }
